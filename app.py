8570bdc06d653836192c38a07b4c792f976cc617
Gtindi <tindigabriel@gmail.com>
2024-07-16 18:33:25 +0300

    Repository updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def db_connection():
     return pymysql.connect(
         host="localhost",
         user="root",
-        password="yourpassword",
+        password="12345",
         db="student_management_system",
         cursorclass=pymysql.cursors.DictCursor
     )
@@ -36,28 +36,114 @@ def manage_students():
         conn.commit()
         return "Student added successfully!", 201
 
-@app.route('/students/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-def manage_student(id):
+# @app.route('/students/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+# def manage_student(id):
+#     conn = db_connection()
+#     cursor = conn.cursor()
+#     if request.method == 'GET':
+#         cursor.execute("SELECT * FROM Student_table WHERE StudentID = %s", (id,))
+#         student = cursor.fetchone()
+#         return jsonify(student)
+#     if request.method == 'PUT':
+#         updated_student = request.json
+#         sql = """UPDATE Student_table SET FirstName=%s, LastName=%s, DOB=%s, Gender=%s, Email=%s, PhoneNumber=%s, Address=%s, StudentTypeID=%s
+#                  WHERE StudentID=%s"""
+#         cursor.execute(sql, (updated_student['FirstName'], updated_student['LastName'], updated_student['DOB'], updated_student['Gender'],
+#                              updated_student['Email'], updated_student['PhoneNumber'], updated_student['Address'], updated_student['StudentTypeID'], id))
+#         conn.commit()
+#         return "Student updated successfully!", 200
+#     if request.method == 'DELETE':
+#         cursor.execute("DELETE FROM Student_table WHERE StudentID = %s", (id,))
+#         conn.commit()
+#         return "Student deleted successfully!", 200
+
+
+# Unit Display
+@app.route('/units', methods=['GET', 'POST'])
+def manage_units():
     conn = db_connection()
     cursor = conn.cursor()
     if request.method == 'GET':
-        cursor.execute("SELECT * FROM Student_table WHERE StudentID = %s", (id,))
-        student = cursor.fetchone()
-        return jsonify(student)
-    if request.method == 'PUT':
-        updated_student = request.json
-        sql = """UPDATE Student_table SET FirstName=%s, LastName=%s, DOB=%s, Gender=%s, Email=%s, PhoneNumber=%s, Address=%s, StudentTypeID=%s
-                 WHERE StudentID=%s"""
-        cursor.execute(sql, (updated_student['FirstName'], updated_student['LastName'], updated_student['DOB'], updated_student['Gender'],
-                             updated_student['Email'], updated_student['PhoneNumber'], updated_student['Address'], updated_student['StudentTypeID'], id))
+        cursor.execute("SELECT * FROM Unit_table")
+        students = cursor.fetchall()
+        return jsonify(students)
+    if request.method == 'POST':
+        new_unit = request.json
+        sql = """INSERT INTO Unit_table (UnitName, UnitCode, CreditHours, LecturerID, FacultyID)
+                 VALUES (%s, %s, %s, %s, %s)"""
+        cursor.execute(sql, (new_unit['UnitName'], new_unit['UnitCode'], new_unit['CreditHours'], new_unit['FacultyID'],))
         conn.commit()
-        return "Student updated successfully!", 200
-    if request.method == 'DELETE':
-        cursor.execute("DELETE FROM Student_table WHERE StudentID = %s", (id,))
-        conn.commit()
-        return "Student deleted successfully!", 200
+        return "Unit added successfully!", 201
 
-# CRUD operations for other tables (Faculty, Lecturer, Unit, StudentType)
+# Accommodation Display
+@app.route('/accommodations', methods=['GET', 'POST'])
+def manage_accommodations():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM Accommodation_table")
+        accommodations = cursor.fetchall()
+        return jsonify(accommodations)
+    if request.method == 'POST':
+        new_accommodations = request.json
+        sql = """INSERT INTO Unit_table (StudentID, RoomNumber, ResidenceName)
+                 VALUES (%s, %s, %s, %s)"""
+        cursor.execute(sql, (new_accommodations['StudentID'], new_accommodations['RoomNumber'], new_accommodations['ResidenceName']))
+        conn.commit()
+        return "Unit added successfully!", 201
+
+# Course Registration Display
+@app.route('/coursereg', methods=['GET', 'POST'])
+def manage_coursereg():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM CourseReg_table")
+        coursereg = cursor.fetchall()
+        return jsonify(coursereg)
+
+# LectureHall Display
+@app.route('/lecturehall', methods=['GET', 'POST'])
+def manage_lecturehall():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM LectureHall_table")
+        lecturehall = cursor.fetchall()
+        return jsonify(lecturehall)
+
+# Faculty Display
+@app.route('/faculty', methods=['GET', 'POST'])
+def manage_faculty():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM Faculty_table")
+        coursereg = cursor.fetchall()
+        return jsonify(coursereg)
+
+# Lecturer Display
+@app.route('/lecturers', methods=['GET', 'POST'])
+def manage_lecturers():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM Lecturer_table")
+        lecturer = cursor.fetchall()
+        return jsonify(lecturer)
+
+# Student Type Display
+@app.route('/studenttype', methods=['GET', 'POST'])
+def manage_studenttypes():
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM StudentType_table")
+        lecturer = cursor.fetchall()
+        return jsonify(lecturer)
+
+
+# CRUD operations for other tables (Faculty, Lecturer, Unit, StudentType, CourseReg, LectureHall, Accommodation)
 @app.route('/<table>', methods=['GET', 'POST'])
 def manage_table(table):
     conn = db_connection()
@@ -96,4 +182,4 @@ def manage_table_record(table, id):
         return f"{table} record deleted successfully!", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
